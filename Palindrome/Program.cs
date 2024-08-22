d591085cb4c3582e2e3b14838cc53198603ceee9
stefka <stefka@DESKTOP-UST16T5>
2024-08-22 15:47:39 +0300

    Add project files.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Palindrome
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = String.Empty;
+            while(command!="END" || command!="End")
+            {
+                Console.WriteLine("Please enter the word you want to check if it is palindrome or END to end the program");
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
