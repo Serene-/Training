943cbabb4238ec2a07db43c1a696a7543769981f
stefka <stefka_123@abv.gb>
2024-08-25 14:48:57 +0300

    Hangman

---
+++ FindAndReplace/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Text;
+using System.Transactions;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Creating a file
+        //Finding and Replacing word in text file.
+        string path = @"C:\Users\stefka\source\repos\Training\FindAndReplace\findAdReplace.txt";
+        Console.WriteLine("Enter the text you want to save in a file.");
+        string text = Console.ReadLine();
+        File.WriteAllText(path,text);
+        Console.WriteLine("Enter the word you want to replace in file.");
+        string word= Console.ReadLine();
+        Console.WriteLine("Enter the new word you want to put in file.");
+        string wordNew = Console.ReadLine();
+        try
+        {
+            StringBuilder file = new StringBuilder();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while ((sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                    // file.AppendLine(sr.ReadLine());
+                }
+            }
+            //file.Replace(word, wordNew);
+            string newFile= file.ToString();
+            Console.WriteLine(file);
+            File.WriteAllText("findAndReplace.txt", newFile);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The file cannot be read");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ Hangman/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Hangman
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string words= @"apple
+plum
+vinegar
+bedroom
+enviroment";
+            string path = @"C:\Users\stefka\source\repos\Training\Hangman\words.txt";
+            File.WriteAllText(path,words);
+            string wordToGuess;
+            Console.WriteLine("Guess the word");
+            try 
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while(!sr.EndOfStream)
+                    {
+                        wordToGuess=sr.ReadLine();
+                        Console.WriteLine("Word:");
+                        GuessingWords(wordToGuess);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+
+        public static void GuessingWords(string wordToGuess)
+        {
+           // char[] word = wordToGuess.ToCharArray();
+            char[] word=new char[wordToGuess.Length];
+            char letter;
+            for (int i = 0; i < wordToGuess.Length; i++)
+            {
+                word[i] = '-';
+            }
+            Console.WriteLine(word);
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"You have {6-i} guesses left.Please enter a letter:");
+                letter = Char.Parse(Console.ReadLine());
+                for (int j = 0; j < wordToGuess.Length; j++)
+                {
+                    if (letter == wordToGuess[j]) word[j] = letter;
+                   
+                }
+                string temp = new string(word);
+                Console.WriteLine(word);
+                if (temp.Equals(wordToGuess))
+                {
+                    Console.WriteLine("Congratulations!!! You guessed the word!");
+                    i = 6;
+                }
+            }
+        }
+
+    }
+}
