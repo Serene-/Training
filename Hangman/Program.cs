78cda3cb0fa8777fe24e93f5e3705af378eb22bd
stefka <stefka_123@abv.gb>
2024-08-26 17:09:20 +0300

    Hangman last version

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -2,41 +2,67 @@
 {
     public class Program
     {
-
+        static string pathDirectory = @"C:\Users\stefka\source\repos\Training\Hangman";
+        static string path;
+        static int score = 0;
+        static int bestScore=0;
+        static StreamReader sr;
         static void Main(string[] args)
         {
-            string words= @"apple
-plum
-vinegar
-bedroom
-enviroment";
-            string path = @"C:\Users\stefka\source\repos\Training\Hangman\words.txt";
-            File.WriteAllText(path,words);
+            CreateFileWithWords();
+            path = Path.Combine(pathDirectory,"words.txt");
             string wordToGuess;
+            Console.WriteLine("Welcome to Hangman!");
             Console.WriteLine("Guess the word");
-            try 
+            try
             {
-                using (StreamReader sr = new StreamReader(path))
+                using (sr = new StreamReader(path))
                 {
-                    while(!sr.EndOfStream)
+                    
+                    while (!sr.EndOfStream)
                     {
-                        wordToGuess=sr.ReadLine();
+                        wordToGuess = sr.ReadLine();
                         Console.WriteLine("Word:");
                         GuessingWords(wordToGuess);
+                        Console.WriteLine("Do you want to continue with the game y/n?");
+                        if (Console.ReadLine().ToLower() == "n") break;
                     }
-                }
+                    path = Path.Combine(pathDirectory, "bestscore.txt");
+                    if(File.Exists(path))
+                    {
+                        using (StreamReader srBestScore=new StreamReader(path))
+                        {
+                            bestScore = Int32.Parse(srBestScore.ReadLine());
+                        }
+                    }
+                    else
+                    {
+                        File.WriteAllText(path, score.ToString());
+
+                    }
+                    if (bestScore <= score)
+                    {
+                        Console.WriteLine($"You have the best score of all time {score}!");
+                        File.WriteAllText(path, score.ToString());
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The best score is {bestScore}, your is {score}!");
+                        File.WriteAllText(path, bestScore.ToString());
+                    }
+                    }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
-                Console.WriteLine("ERROR");
+                Console.WriteLine(e.Message);
             }
         }
 
         public static void GuessingWords(string wordToGuess)
         {
-           // char[] word = wordToGuess.ToCharArray();
-            char[] word=new char[wordToGuess.Length];
+            char[] word = new char[wordToGuess.Length];
             char letter;
+            string guessedWord=null;
             for (int i = 0; i < wordToGuess.Length; i++)
             {
                 word[i] = '-';
@@ -44,22 +70,40 @@ enviroment";
             Console.WriteLine(word);
             for (int i = 0; i < 6; i++)
             {
-                Console.WriteLine($"You have {6-i} guesses left.Please enter a letter:");
-                letter = Char.Parse(Console.ReadLine());
-                for (int j = 0; j < wordToGuess.Length; j++)
-                {
-                    if (letter == wordToGuess[j]) word[j] = letter;
-                   
-                }
-                string temp = new string(word);
-                Console.WriteLine(word);
-                if (temp.Equals(wordToGuess))
-                {
-                    Console.WriteLine("Congratulations!!! You guessed the word!");
-                    i = 6;
-                }
+                Console.WriteLine($"You have {6 - i} guesses left.Please enter a letter:");
+                   letter = Char.Parse(Console.ReadLine());
+                    for (int j = 0; j < wordToGuess.Length; j++)
+                    {
+                        if (letter == wordToGuess[j]) word[j] = letter;
+
+                    }
+                    guessedWord = new string(word);
+                    Console.WriteLine(word);
+                    if (guessedWord.Equals(wordToGuess))
+                    {
+                        Console.WriteLine("Congratulations!!! You guessed the word!");
+                        score++;
+                        Console.WriteLine($"Your score is {score}");
+                        i = 6;
+                    }              
+            }
+            if (!guessedWord.Equals(wordToGuess))
+            {
+                Console.WriteLine($"You didn't guess the word. The word was {wordToGuess}");
             }
         }
+
+        public static void CreateFileWithWords()
+        {
+            string words = @"apple
+plum
+pine
+bag
+smell";
+            string path = Path.Combine(pathDirectory, "words.txt");
+            File.WriteAllText(path, words);
+        }
+
 
     }
 }
