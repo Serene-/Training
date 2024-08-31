e3f48f521cffa640de98f9f55c30e1f1920467f8
stefka <stefka_123@abv.gb>
2024-08-31 13:34:56 +0300

    version1

---
+++ FirstUniqueCharacter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FirstUniqueCharacter
+{
+    public class Program
+    {
+        //Given a string s, find the first non-repeating character 
+        //in it and return its index.If it does not exist, return -1.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string word = Console.ReadLine();
+            Console.WriteLine(FirstUniqChar(word));
+        }
+        public static int FirstUniqChar(string s)
+        {
+            char[] array = s.ToCharArray();
+            int found = 0;
+            int index = -1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] == array[j]) found++;
+                }
+                if (found == 1)
+                {
+                    index = i;
+                    break;
+                }
+                found = 0;
+            }
+            return index;
+        }
+    }
+}
+
