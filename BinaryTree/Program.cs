6111eeac82872f24e31f543197ef33fa6fbc2423
stefka <stefka_123@abv.gb>
2024-09-02 14:35:45 +0300

    initial commit

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace BinaryTree
+{
+    public class Program
+    {
+        //Given the root of a binary tree, return the sum of all left leaves.
+        //A leaf is a node with no children.
+        //A left leaf is a leaf that is the left child of another node.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ JumpGame/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace JumpGame
+{
+    /*You are given an integer array nums. 
+     * You are initially positioned at the array's first index, 
+     * and each element in the array represents your maximum jump length 
+     * at that position.
+     * Return true if you can reach the last index, or false otherwise.*/
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an arary seperated by comma:");
+            int[] nums = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+            Console.WriteLine($"{CanJump(nums)}");
+
+        }
+        public static bool CanJump(int[] nums)
+        {
+            bool canJump = false;
+            int index = 0;
+            while(index< nums.Length)
+            {
+                index = index+nums[index];
+                if (index==nums.Length-1)
+                {
+                    canJump = true;
+                    break;
+                }
+                if (index < 0 || index >= nums.Length || nums[index]==0)
+                {
+                    break;
+                }
+            }
+            return canJump;
+        }
+    }
+}
